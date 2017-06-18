55d578c92bef844f40e3e98b4930642905e96ec4
afeiship <1290657123@qq.com>
2017-06-18 11:00:34 +0800

    ntaila

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,32 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-(function () {
-  var ManifestWebpackPlugin = function () {
-    function ManifestWebpackPlugin() {
-      _classCallCheck(this, ManifestWebpackPlugin);
+var ManifestWebpackPlugin = function () {
+  function ManifestWebpackPlugin() {
+    _classCallCheck(this, ManifestWebpackPlugin);
 
-      this.onDone = function (inStata) {
-        console.log('========= instate start ==============');
-        conosole.log(inStata);
-        console.log('========= instate end  ==============');
-      };
+    this.onDone = function (inStata) {
+      console.log('========= instate start ==============');
+      conosole.log(inStata);
+      console.log('========= instate end  ==============');
+    };
+  }
+
+  _createClass(ManifestWebpackPlugin, [{
+    key: 'apply',
+    value: function apply(compiler) {
+      compiler.plugin('done', this.onDone);
     }
+  }]);
 
-    _createClass(ManifestWebpackPlugin, [{
-      key: 'apply',
-      value: function apply(compiler) {
-        compiler.plugin('done', this.onDone);
-      }
-    }]);
+  return ManifestWebpackPlugin;
+}();
 
-    return ManifestWebpackPlugin;
-  }();
-})();
+exports.default = ManifestWebpackPlugin;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-(function(){
-
-
-  class ManifestWebpackPlugin{
-    apply(compiler){
-      compiler.plugin('done',this.onDone);
-    }
-
-    onDone = (inStata) =>{
-      console.log('========= instate start ==============');
-      conosole.log(inStata);
-      console.log('========= instate end  ==============');
-    };
+export default class ManifestWebpackPlugin{
+  apply(compiler){
+    compiler.plugin('done',this.onDone);
   }
 
-}());
+  onDone = (inStata) =>{
+    console.log('========= instate start ==============');
+    conosole.log(inStata);
+    console.log('========= instate end  ==============');
+  };
+}
+
